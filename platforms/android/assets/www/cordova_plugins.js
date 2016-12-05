ec6efa6da71389ec833fd798ecd77a6bc58819ae
Anshu2502 <anshulkhare50@gmail.com>
2016-12-05 12:12:31 +0530

    Project on Phonegap
    Online tailor app

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-fcm.FCMPlugin",
+        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
+        "pluginId": "cordova-plugin-fcm",
+        "clobbers": [
+            "FCMPlugin"
+        ]
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-fcm": "1.1.5",
+    "cordova-plugin-whitelist": "1.3.0",
+    "phonegap-plugin-barcodescanner": "6.0.3"
+};
+// BOTTOM OF METADATA
+});
